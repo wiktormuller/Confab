224df78b0cab77fe48e4a0142767e5eaefcd1b9e
Wiktor Muller <biznes.wiktor@gmail.com>
2022-11-29 20:06:33 +0100

    feat(general): Add general structure for project with empty projects

---
+++ src/Bootstraper/Confab.Bootstraper/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapGet("/", () => "Confab API!");
+});
+
+app.Run();
